d607b803e114bf5f5f7527fee8979878992670f3
Synergetic00 <evanjgillard@gmail.com>
2021-03-26 10:39:08 +1100

    General points started on prereqs

--- parser/Prereqs.py
+++ parser/Prereqs.py
@@ -1,9 +1,14 @@
 import json
+import re
 
 def parseTrim(string):
     return string.replace('<p>','').replace('</p>','').replace('\u00a0',' ').replace('<div>\\n','').replace('<div>\n','').replace('<br />','').replace("\u2018", "'").replace("\u2019", "'").replace("\u2013", "-").replace("&amp;", "&").strip()
 
 output = {}
+
+# Regex
+cps = "\w+ at \w+ level or above"
+adm = "^Admission to .*"
 
 # Read from file
 with open('data/units.json') as infile:
@@ -12,7 +17,33 @@ with open('data/units.json') as infile:
 for unit in data:
     output[unit] = {}
     if 'prerequisite' in data[str(unit)]:
-        output[unit]['original'] = parseTrim(data[str(unit)]['prerequisite'])
+
+        append = True
+
+        original = parseTrim(data[str(unit)]['prerequisite'])
+
+        if re.match(adm, original):
+            print(original)
+        
+        if re.match(cps, original):
+            if re.match('^'+cps+'$', original):
+                output[unit]['generalCredits'] = {}
+                cpsArgs = original.split(' ')
+                output[unit]['generalCredits']['minCredits'] = re.sub(r'[a-z]+', '', cpsArgs[0], re.I)
+                output[unit]['generalCredits']['minLevel'] = re.sub(r'[a-z]+', '', cpsArgs[2], re.I)
+                append = False
+                #print(original)
+            if 'including' in original:
+                includes = original.split(' including ')
+                output[unit]['generalCredits'] = {}
+                cpsArgs = includes[0].split(' ')
+                output[unit]['generalCredits']['minCredits'] = re.sub(r'[a-z]+', '', cpsArgs[0], re.I)
+                output[unit]['generalCredits']['minLevel'] = re.sub(r'[a-z]+', '', cpsArgs[2], re.I)
+                #print(includes)
+        
+        # Save original string to file
+        if append:
+            output[unit]['original'] = original
 
 # Write to file
 with open('data/prereqs.json', 'w') as outfile:
