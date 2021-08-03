5107b07181227e3cf85a6cc9806418c457c6d4c6
Synergetic00 <synergetic00@gmail.com>
2021-08-03 18:07:09 +1000

    Sorted by length

--- parser/Parser.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('data/prereqs.json') as infile:
-    data = json.load(infile)
-
-output = sorted(data.items(), key=lambda x: len(x[1]['original']))
-
-with open('data/prereqs.json', 'w') as outfile:
-    json.dump(output, outfile, indent=4)
