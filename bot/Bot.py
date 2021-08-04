e40112a894d1370bd8238fe41923dc4357aadb45
Synergetic00 <synergetic00@gmail.com>
2021-08-04 12:41:16 +1000

    Bot stuff

--- bot/Bot.py
+++ bot/Bot.py
@@ -1,15 +1,56 @@
-import os
-
 import discord
-from dotenv import load_dotenv
+import os
+import json
+from discord.ext import commands
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+with open('bot/config.json') as infile:
+    config = json.load(infile)
 
-client = discord.Client()
+with open('data/units.json') as infile:
+    units = json.load(infile)
+
+with open('data/courses.json') as infile:
+    courses = json.load(infile)
+
+client = commands.Bot(command_prefix=config['prefix'])
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('We have logged in as {0.user}'.format(client))
 
-client.run(TOKEN)
+@client.command()
+async def info(ctx, *, args):
+    for arg in args.split():
+        unitcode = arg.upper()
+        if unitcode in units:
+            unitinfo = units[unitcode]
+            embedVar = discord.Embed(
+                title=unitcode,
+                description=unitinfo['description'],
+                color=0xf52c40
+            )
+            learningoutcomes = ''
+            for outcome in unitinfo['outcomes'].values():
+                learningoutcomes += '• ' + outcome + '\n'
+            embedVar.add_field(
+                name="Learning Outcomes",
+                value=learningoutcomes.strip()
+            )
+            offerings = set()
+            for offering in unitinfo['offerings'].values():
+                offerings.add(offering['period'])
+            offeredperiods = ''
+            for offering in sorted(offerings):
+                offeredperiods += '• ' + offering + '\n'
+            embedVar.add_field(
+                name="Offering Periods",
+                value=offeredperiods.strip()
+            )
+            await ctx.send(embed=embedVar)
+
+@info.error
+async def info_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+        await ctx.send("Put course or unit code(s) after command")
+
+client.run(config['discord'])
