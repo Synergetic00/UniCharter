6077ffd4ffa1c23ebc4c0764e86c59bec9e6102f
Synergetic00 <synergetic00@gmail.com>
2022-02-11 01:14:26 +1100

    Gotten prereq parser to get tokens needed

--- parsers/prereqs.py
+++ parsers/prereqs.py
@@ -5,18 +5,47 @@ with open('data/units.json') as file:
     data = json.load(file)
 
 codere = '[A-Z]{4}[0-9]{4}'
-
 output = {}
+
+def splitPrereqs(prereq, tokens):
+    output = []
+    parsed = prereq
+    parsed = parsed.replace('(or', 'or (') # fix a single error
+    parsed = re.sub(r' ?\(|\)| or | and ', '|', parsed)
+    parsed = parsed.split('|')
+    output.append(parsed[0])
+    for i in range(len(tokens)):
+        output.append(tokens[i].strip())
+        output.append(parsed[i+1].strip())
+    output = list(filter(None, output))
+    return output
+
+highest = []
 
 for entry in data.items():
     unitcode = entry[0]
     output[unitcode] = {}
 
-    prereqs = 'None'
     if 'prerequisite' in data[unitcode]:
         prereqs = entry[1]['prerequisite']
-    tokens = re.findall(r'\)|\(|or|and', prereqs)
+        # prevent parsing errors
+        prereqs = prereqs.replace('and above', 'minimum')
+        prereqs = prereqs.replace('or above', 'minimum')
+        prereqs = prereqs.replace('(P)', '>= 50')
+        prereqs = prereqs.replace('(Cr)', '>= 65')
+        prereqs = prereqs.replace('(D)', '>= 75')
+        prereqs = prereqs.replace('(HD)', '>= 85')
+    else:
+        output[unitcode]['prereqs'] = 'None'
+        continue # don't go futher, nothing to analyse
+    tokens = re.findall(r'\)|\(| or | and ', prereqs)
+    tokens = [token.strip() for token in tokens]
+    arr = splitPrereqs(prereqs, tokens)
+    highest.append([len(arr), unitcode, arr])
     output[unitcode]['prereqs'] = prereqs
+
+sorted = sorted(highest, key=lambda x:x[0])
+print(sorted[-1][2])
     
 with open('data/prereqs.json', 'w') as outfile:
     json.dump(output, outfile, indent=4, sort_keys=True)
--- run.sh
+++ run.sh
@@ -1,11 +1,19 @@
-FORCE_UPDATE=true
+FORCE_UPDATE=false
+
+pyrun() {
+    if [ "$(uname)" == "Darwin" ]; then
+        python3 "scrapers/$1.py"
+    else
+        python "scrapers/$1.py"
+    fi
+}
 
 if [ ! -e 'data/courses.json' ] || $FORCE_UPDATE ; then
     echo 'Downloaded courses json file'
-    python scrapers/courses.py
+    pyrun "courses"
 fi
 
 if [ ! -e 'data/units.json' ] || $FORCE_UPDATE ; then
     echo 'Downloaded unit json file'
-    python scrapers/units.py
+    pyrun "units"
 fi
