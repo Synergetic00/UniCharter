cfa62536ecb20b058abad50e0d759924a9ab9380
Synergetic00 <evanjgillard@gmail.com>
2022-02-22 16:36:06 +1100

    Finished prerequisite parser

--- parsers/prereqs.py
+++ parsers/prereqs.py
@@ -9,13 +9,13 @@ output = {}
 
 def splitPrereqs(prereq, tokens):
     output = []
-    parsed = prereq
+    parsed = str(prereq)
     parsed = parsed.replace('(or', 'or (') # fix a single error
     parsed = re.sub(r' ?\(|\)| or | and | including', '|', parsed)
     parsed = parsed.split('|')
     output.append(parsed[0])
     for i in range(len(tokens)):
-        output.append(tokens[i].strip())
+        output.append(str(tokens[i]).strip())
         output.append(parsed[i+1].strip())
     output = list(filter(None, output))
     return output
@@ -35,15 +35,16 @@ def getDepthParsedArr(arr):
     strconv = strconv.replace('[, "', '["')
     strconv = strconv.replace(', ]', ']')
     strconv = strconv.replace('[, [', '[[')
-    # print(strconv)
     output = list(json.loads(strconv))
     return output
 
-for entry in data.items():
+for entry in dict(data).items():
     unitcode = entry[0]
     output[unitcode] = {}
     if 'prerequisite' in data[unitcode]:
-        prereqs = entry[1]['prerequisite']
+        prereqs = str(entry[1]['prerequisite'])
+        prereqs = prereqs.replace('[', '(')
+        prereqs = prereqs.replace(']', ')')
         # prevent parsing errors
         prereqs = prereqs.replace('and above', 'minimum')
         prereqs = prereqs.replace('or above', 'minimum')
@@ -59,6 +60,7 @@ for entry in data.items():
         prereqs = prereqs.replace('(ECE)', ' - EarlyChildhood ')
         prereqs = prereqs.replace('OR', 'or')
         prereqs = prereqs.replace('AND', 'and')
+        prereqs = re.sub(r'  +', ' ', prereqs)
         # single parsing errors
         prereqs = prereqs.replace('(ANTH150 or ANTH1050) or (40cp at 1000 level minimum', '(ANTH150 or ANTH1050) or 40cp at 1000 level minimum') # ANTH2003
         prereqs = prereqs.replace('(BIOL8770 or BIOL877) or BIOL8870 or BIOL887)', '(BIOL8770 or BIOL877) or (BIOL8870 or BIOL887)') # BIOL8710
@@ -67,32 +69,22 @@ for entry in data.items():
         prereqs = prereqs.replace('(((TEP401 or EDTE4010 - S)', '((TEP401 or EDTE4010 - S)')
         prereqs = prereqs.replace('((ELCT3005 or ELEC395)', '(ELCT3005 or ELEC395)') # ELEC4092
         prereqs = prereqs.replace('130cp at 1000 level minimum including HLTH200 or HLTH2000) or 130cp and admission to BHumanSc', '(130cp at 1000 level minimum including HLTH200 or HLTH2000) or (130cp and admission to BHumanSc)') # HLTH3100
-        # LAWS5023 - (MMCC2014 or MAS214) or (BUSL250 and BUSL301), (MMCC2014 or MAS214) or BUSL250 and BUSL301)
-        # LAWS5036
-        # LAWS8018
-        # PSYU2235
-        # PSYU2236
-        # PSYU3349
-        # PSYU3351
-        # PSYU3399
-        # PSYX2235
-        # PSYX2236
-        # SPED8901
-        # STAT3102
-        # STAT8111
-        prereqs = prereqs.replace('(((TEP401', '((TEP401')
-        prereqs = re.sub(r'  +', ' ', prereqs)
+        prereqs = prereqs.replace('(MMCC2014 or MAS214) or (BUSL250 and BUSL301)', '(MMCC2014 or MAS214) or BUSL250 and BUSL301)') # LAWS5023
+        prereqs = prereqs.replace('Admission to LLM or MIntTrdeComLaw or M', '(Admission to LLM or MIntTrdeComLaw or M') # LAWS8018
+        prereqs = prereqs.replace('BSpHScBPsych - Honours or DipSphComm)', 'BSpHScBPsych - Honours or DipSphComm))') # PSYU2235
+        prereqs = prereqs.replace('STAX1103)) or ((COGS100 or COGS1000) or (MEDI204', 'STAX1103) or ((COGS100 or COGS1000) or (MEDI204') # PSYU2236
+        prereqs = prereqs.replace('or PSY248 or PSYU2248 or PSYX248 or PSYX2248))', 'or PSY248 or PSYU2248 or PSYX248 or PSYX2248)))') # PSYU3349
+        prereqs = prereqs.replace('or BHumanSc))', 'or BHumanSc)') # PSYU3399
+        prereqs = prereqs.replace('SPED8260)', 'SPED8260') # SPED8901
+        prereqs = prereqs.replace('PSYU2248))', 'PSYU2248)))') # STAT3102
+        prereqs = prereqs.replace('ECON8040))or', 'ECON8040) or') # STAT8111
     else:
         output[unitcode]['prereqs'] = 'None'
         continue # don't go futher, nothing to analyse
-    output[unitcode]['prereqs'] = prereqs
-    # print(unitcode)
-    try:
-        arr = getParsedArr(prereqs)
-        depthArr = getDepthParsedArr(arr)
-    except:
-        print('Error: ' + unitcode)
-    # output[unitcode]['prereqs'] = depthArr
+    # output[unitcode]['prereqs'] = prereqs
+    arr = getParsedArr(prereqs)
+    depthArr = getDepthParsedArr(arr)
+    output[unitcode]['prereqs'] = depthArr
 
 with open('data/prereqs.json', 'w') as outfile:
     json.dump(output, outfile, indent=4, sort_keys=True)
--- run.sh
+++ run.sh
@@ -1,19 +1,24 @@
-FORCE_UPDATE=true
+FORCE_UPDATE=false
 
 pyrun() {
     if [ "$(uname)" == "Darwin" ]; then
-        python3 "scrapers/$1.py"
+        python3 "$1.py"
     else
-        python "scrapers/$1.py"
+        python "$1.py"
     fi
 }
 
 if [ ! -e 'data/courses.json' ] || $FORCE_UPDATE ; then
     echo 'Downloaded courses json file'
-    pyrun "courses"
+    pyrun "scrapers/courses"
 fi
 
 if [ ! -e 'data/units.json' ] || $FORCE_UPDATE ; then
     echo 'Downloaded unit json file'
-    pyrun "units"
+    pyrun "scrapers/units"
+fi
+
+if [ ! -e 'data/prereqs.json' ] || $FORCE_UPDATE ; then
+    echo 'Parsed unit for prerequisites'
+    pyrun "parsers/prereqs"
 fi
